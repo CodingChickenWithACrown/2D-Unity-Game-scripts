e6d7d674534d492c921f46fb19b5b36d13938e12
Chicken_King632 <dataprotectionorg@gmail.com>
2025-11-30 15:46:30 -0500

    Refactor collision detection for player death

--- FrogDeath.cs
+++ FrogDeath.cs
@@ -7,9 +7,13 @@ public class FrogDeath : MonoBehaviour
     public Animator animator;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player"){
-            Destroy(gameObject, 1);
-            animator.SetTrigger("Dies");
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("Dies");
+            }
+            Destroy(gameObject, 1f);
         }
     }
 }
