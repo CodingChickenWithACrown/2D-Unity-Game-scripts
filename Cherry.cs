80f83083ac3fd5985314ded51727216ea2b76db1
Chicken_King632 <dataprotectionorg@gmail.com>
2025-11-30 15:45:18 -0500

    Use CompareTag for player collision check

--- Cherry.cs
+++ Cherry.cs
@@ -6,7 +6,7 @@ public class Cherry : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
